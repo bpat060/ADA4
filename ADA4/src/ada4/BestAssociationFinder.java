4e82c9c725a2bffc39d296fc6c1fe7651bbaef3f
Bhakti Patel <87521790+bpat060@users.noreply.github.com>
2021-10-21 12:19:27 +1300

    dgbcx

--- ADA4/src/ada4/BestAssociationFinder.java
+++ ADA4/src/ada4/BestAssociationFinder.java
@@ -9,13 +9,6 @@ package ada4;
  *
  * @author Owner
  */
-public class ADA4 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class BestAssociationFinder {
 
 }
