6b7e25e79d84911fa4942f3a04cf14a1ebb52902
sheetalskumar <84754762+sheetalskumar@users.noreply.github.com>
2021-10-21 13:10:28 +1300

    Update

---
+++ ADA4/src/useful/RouterDisplay.java
@@ -0,0 +1,19 @@
+package useful;
+
+/**
+   An interface that represents a display for the UnicastRouter
+   @see UnicastRouter.java
+*/
+import java.net.InetAddress;
+import java.util.Map;
+
+public interface RouterDisplay
+{
+   // notification to display the string text
+   public void displayText(String text);
+   // notification to display updated adjacent routers
+   public void displayAdjacentRouters
+      (Map<InetAddress, RouterConnection> adjacentRouters);
+   // notification to display updated network routers
+   public void displayNetwork(RouterDistanceVector distanceVector);
+}
